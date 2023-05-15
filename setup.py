47a6770954974327944d3c6b7c70f094abeab2c1
Nick Träger <nick.traeger@ideler.de>
2023-05-15 17:08:01 +0200

    setup.py created

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="strtotime",
+    version="0.0.1",
+    url="https://github.com/ideler/strtotime-windows-dummy",
+    license="MIT",
+    author="Ideler IT-Service GmbH",
+    author_email="hosting@ideler.de",
+    description="Dummy strtotime module for windows",
+    py_modules=["strtodatetime"],
+)
